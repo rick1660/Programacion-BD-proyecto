da76cc6bc403885276b2f32c6029fac6747db709
Ricardo <ricardo.jacuinde16@tectijuana.edu.mx>
2020-10-09 13:31:07 -0700

    Add Info tablas cliente automovil departamento

--- SQL/ProyectoBD.sql
+++ SQL/ProyectoBD.sql
@@ -102,3 +102,37 @@ CONSTRAINT FK_Refaccion FOREIGN KEY (IdRefaccion) REFERENCES Refaccion(IdRefacci
 CONSTRAINT FK_IdServicio FOREIGN KEY (IdServicio) REFERENCES Servicios(IdServicio)
 )
 
+
+
+
+
+
+
+select * from Cliente;	 
+
+
+insert into Cliente values (1,'Alfreds Futterkiste', 'Maria Anders','Obere Str. 57 Berlin','6645214783','alfreds@gmail.com' )
+insert into Cliente values (2,'Ana', 'Trujillo Truenos','Avda. de la Constitución 2222', '6648744521','Ana@gmail.com')
+insert into Cliente values (3,'Antonio', 'Moreno Marquez','Mataderos 2312', '6648796322','Antonio@gmail.com')
+insert into Cliente values (4,'Celsa', 'Piña Valderrama','avda. la cotorra', '6647789654','celsa@gmail.com')
+insert into Cliente values (5,'Pablo', 'Perez Pulido','avda el penon', '6648996542','pablo@gmail.com')
+
+
+insert into Automovil values(1,8741,'Nissan','Altima','gris',2012, 2020-09-27 ,1)
+insert into Automovil values(2,7784,'Honda','civic','Morado',1998, 2020-09-30 ,2)
+insert into Automovil values(3,4521,'Ford','Fiesta','rojo',2006,2020-10-1,3)
+insert into Automovil values(4,4587,'Volkswagen','Jetta','amarillo',2017,2020-10-3,4)
+insert into Automovil values(5,1235,'Chevrolet','Camaro','blanco',2019,2020-10-4,5)
+insert into Automovil values(6,4521,'Ford','Focus','Negro',2004,2020-10-6,3)
+insert into Automovil values(7,1235,'Lambirghini','Aventor','Azul',2020,2020-09-13,2)
+insert into Automovil values(8,4587,'Maserati','Gran turismo','Plateado',2018,2020-06-8,2)
+
+select * from Automovil
+
+
+Insert into Departamentos values(1,'Carroceria',3)
+Insert into Departamentos values(2,'Electrico',5)
+Insert into Departamentos values(3,'Suspencion',2)
+Insert into Departamentos values(4,'Mecanico',1)
+Insert into Departamentos values(5,'Refaccionari',4)
+
