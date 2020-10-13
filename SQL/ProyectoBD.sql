83643741389c5981a8aa438d214d71e3d434bfdf
Ricardo <ricardo.jacuinde16@tectijuana.edu.mx>
2020-10-12 21:16:14 -0700

    Add Info tablas

--- SQL/ProyectoBD.sql
+++ SQL/ProyectoBD.sql
@@ -33,6 +33,9 @@ IdVehiculo int,
 CONSTRAINT FK_IdVehiculo_TBEmpleado FOREIGN KEY (IdVehiculo) REFERENCES Automovil(IdVehiculo)
 );
 
+Drop table Empleado;
+select * from Empleado;
+
 create table Diagnostico(
 IdDiagnostico int PRIMARY KEY NOT NULL,
 Falla varchar (30),
@@ -97,15 +100,20 @@ Total float,
 IdCliente int,
 IdServicio int,
 IdRefaccion int,
+IdEmpleado int,
 CONSTRAINT FK_IdCliente2 FOREIGN KEY (IdCliente) REFERENCES Cliente(IdCliente),
-CONSTRAINT FK_Refaccion FOREIGN KEY (IdRefaccion) REFERENCES Refaccion(IdRefaccion),
-CONSTRAINT FK_IdServicio FOREIGN KEY (IdServicio) REFERENCES Servicios(IdServicio)
+CONSTRAINT FK_Refaccion  FOREIGN KEY (IdRefaccion) REFERENCES Refaccion(IdRefaccion),
+CONSTRAINT FK_IdServicio FOREIGN KEY (IdServicio) REFERENCES Servicios(IdServicio),
+CONSTRAINT FK_Empleado   FOREIGN KEY (IdEmpleado) REFERENCES Empleado(IdEmpleado)
 )
 
 
 
 
-select * from Cliente;	 
+
+
+select * from Factura;	 
+
 
 
 insert into Cliente values (1,'Alfreds Futterkiste', 'Maria Anders','Obere Str. 57 Berlin','6645214783','alfreds@gmail.com' )
@@ -114,7 +122,7 @@ insert into Cliente values (3,'Antonio', 'Moreno Marquez','Mataderos 2312', '664
 insert into Cliente values (4,'Celsa', 'Piña Valderrama','avda. la cotorra', '6647789654','celsa@gmail.com')
 insert into Cliente values (5,'Pablo', 'Perez Pulido','avda el penon', '6648996542','pablo@gmail.com')
 
-select * from Automovil
+
 insert into Automovil values(1,8741,'Nissan','Altima','gris',2012, 2020-09-27 ,1)
 insert into Automovil values(2,7784,'Honda','civic','Morado',1998, 2020-09-30 ,2)
 insert into Automovil values(3,4521,'Ford','Fiesta','rojo',2006,2020-10-1,3)
@@ -123,7 +131,7 @@ insert into Automovil values(5,1235,'Chevrolet','Camaro','blanco',2019,2020-10-4
 insert into Automovil values(6,4521,'Ford','Focus','Negro',2004,2020-10-6,3)
 insert into Automovil values(7,1235,'Lambirghini','Aventor','Azul',2020,2020-09-13,2)
 insert into Automovil values(8,4587,'Maserati','Gran turismo','Plateado',2018,2020-06-8,2)
-
+select * from Automovil;
 
 
 
@@ -136,43 +144,101 @@ Insert into Departamentos values(5,'Refaccionari',7358)
 
 
 
+create table Empleado(
+IdEmpleado int primary key NOT NULL,
+Nombre varchar(40),
+Telefono varchar(10),
+IdDepartamento int,
+IdVehiculo int,
+CONSTRAINT FK_IdVehiculo_TBEmpleado FOREIGN KEY (IdVehiculo) REFERENCES Automovil(IdVehiculo)
+);
+
+
+select * from Empleado;
+
+
 --ALE
 --Empleados
 --Carrocería y pintura 01
-insert into Empleado(IdEmpleado,Nombre,Telefono,IdDepartamento,IdVehiculo) values(0123,'Francisco Camacho',667456213,01,345);
-insert into Empleado(IdEmpleado,Nombre,Telefono,IdDepartamento,IdVehiculo) values(0124,'Santiago Ortega',667789411,01,242);
-insert into Empleado(IdEmpleado,Nombre,Telefono,IdDepartamento,IdVehiculo) values(0125,'Joel Molina',667279171,01,120);
+  insert into Empleado values(0123,'Francisco Camacho',667456213,01,1);
+--insert into Empleado(IdEmpleado,Nombre,Telefono,IdDepartamento,IdVehiculo) values(0124,'Santiago Ortega',667789411,01,242);
+--insert into Empleado(IdEmpleado,Nombre,Telefono,IdDepartamento,IdVehiculo) values(0125,'Joel Molina',667279171,01,120);
 
 --Eléctrico 02
-insert into Empleado(IdEmpleado,Nombre,Telefono,IdDepartamento,IdVehiculo) values(0223,'Israel Trujillo',667654321,02,771);
-insert into Empleado(IdEmpleado,Nombre,Telefono,IdDepartamento,IdVehiculo) values(0224,'Daniel Sanchez',667741258,02,305);
-insert into Empleado(IdEmpleado,Nombre,Telefono,IdDepartamento,IdVehiculo) values(0225,'Mario Gomez',667321456,02,471);
+  insert into Empleado values (0223,'Israel Trujillo',667654321,02,4);
+--insert into Empleado(IdEmpleado,Nombre,Telefono,IdDepartamento,IdVehiculo) values(0224,'Daniel Sanchez',667741258,02,305);
+--insert into Empleado(IdEmpleado,Nombre,Telefono,IdDepartamento,IdVehiculo) values(0225,'Mario Gomez',667321456,02,471);
 
 --Alineación y balanceo, suspensión 03
-insert into Empleado(IdEmpleado,Nombre,Telefono,IdDepartamento,IdVehiculo) values(0323,'Diego Lopez',667357159,03,153);
-insert into Empleado(IdEmpleado,Nombre,Telefono,IdDepartamento,IdVehiculo) values(0324,'Jonathan Martinez',667971364,03,682);
-insert into Empleado(IdEmpleado,Nombre,Telefono,IdDepartamento,IdVehiculo) values(0325,'Simon Velasquez',667326748,03,024);
+  insert into Empleado(IdEmpleado,Nombre,Telefono,IdDepartamento,IdVehiculo) values(0323,'Diego Lopez',667357159,03,5);
+/*insert into Empleado(IdEmpleado,Nombre,Telefono,IdDepartamento,IdVehiculo) values(0324,'Jonathan Martinez',667971364,03,682);
+insert into Empleado(IdEmpleado,Nombre,Telefono,IdDepartamento,IdVehiculo) values(0325,'Simon Velasquez',667326748,03,024)*/;
 
 --Mecánico 04
-insert into Empleado(IdEmpleado,Nombre,Telefono,IdDepartamento,IdVehiculo) values(0423,'Julio Calderon',664355412,04,910);
-insert into Empleado(IdEmpleado,Nombre,Telefono,IdDepartamento,IdVehiculo) values(0424,'Santiago Muñoz',667369852,04,547);
-insert into Empleado(IdEmpleado,Nombre,Telefono,IdDepartamento,IdVehiculo) values(0425,'Ricardo Cebreros',667147987,04,241);
+  insert into Empleado(IdEmpleado,Nombre,Telefono,IdDepartamento,IdVehiculo) values(0423,'Julio Calderon',664355412,04,2);
+/*insert into Empleado(IdEmpleado,Nombre,Telefono,IdDepartamento,IdVehiculo) values(0424,'Santiago Muñoz',667369852,04,547);
+insert into Empleado(IdEmpleado,Nombre,Telefono,IdDepartamento,IdVehiculo) values(0425,'Ricardo Cebreros',667147987,04,241)*/;
 
 --Refaccionaria 05
-insert into Empleado(IdEmpleado,Nombre,Telefono,IdDepartamento,IdVehiculo) values(0523,'Saul Sandoval',667753211,05,301);
-insert into Empleado(IdEmpleado,Nombre,Telefono,IdDepartamento,IdVehiculo) values(0524,'Bryan Perez',667108535,05,852);
-insert into Empleado(IdEmpleado,Nombre,Telefono,IdDepartamento,IdVehiculo) values(0525,'Lucas Acosta',667997711,05,151);
+  insert into Empleado(IdEmpleado,Nombre,Telefono,IdDepartamento,IdVehiculo) values(0523,'Saul Sandoval',667753211,05,3);
+/*insert into Empleado(IdEmpleado,Nombre,Telefono,IdDepartamento,IdVehiculo) values(0524,'Bryan Perez',667108535,05,852);
+insert into Empleado(IdEmpleado,Nombre,Telefono,IdDepartamento,IdVehiculo) values(0525,'Lucas Acosta',667997711,05,151)*/;
+
+create table Diagnostico(
+IdDiagnostico int PRIMARY KEY NOT NULL,
+Falla varchar (30),
+Causa varchar (30),
+IdVehiculo int,
+CONSTRAINT FK_IdVehiculo_TBDiagnostico FOREIGN KEY (IdVehiculo) REFERENCES Automovil(IdVehiculo)
 
 --Diagnostico
-insert into Diagnostico(IdDiagnostico,Falla,Causa,IdVehiculo) values(0012,'Al manejar el carro se direcciona de mal manera','Alineacion de las llantas',682);
-insert into Diagnostico(IdDiagnostico,Falla,Causa,IdVehiculo) values(0013,'No se prendren las luces del frente','Luces',471);
-insert into Diagnostico(IdDiagnostico,Falla,Causa,IdVehiculo) values(0014,'Se escucha un sonido raro en el cofre','Motor',241);
+insert into Diagnostico(IdDiagnostico,Falla,Causa,IdVehiculo) values(0012,'no direcciona bien','Alineacion de las llantas',5);
+insert into Diagnostico(IdDiagnostico,Falla,Causa,IdVehiculo) values(0013,'luces no prenden','Luces',4);
+insert into Diagnostico(IdDiagnostico,Falla,Causa,IdVehiculo) values(0014,'sonido raro cofre','Motor',1);
+
+
+
+
+
+insert into Automovil values(5,1235,'Chevrolet','Camaro','blanco',2019,2020-10-4,5)
+insert into Automovil values(4,4587,'Volkswagen','Jetta','amarillo',2017,2020-10-3,4)
+insert into Automovil values(1,8741,'Nissan','Altima','gris',2012, 2020-09-27 ,1)
+
+insert into Servicios values (1,5000,2020-10-04,'alineacion',10,1,2020-10-04,2020-11-04, 0012, 3 )
+insert into Servicios values (2,3000,2020-10-03,'Focos',10,1,2020-10-03,2020-11-03, 0013, 2 )
+insert into Servicios values (3,1000,2020-09-27,'arreglo Motor ',10,1,2020-09-27,2020-10-27, 0014, 1 )
+
+select * from servicios;
+
+create table Factura(
+IdFactura int PRIMARY KEY NOT NULL,
+Fecha Datetime,
+Total float,
+IdCliente int,
+IdServicio int,
+IdRefaccion int,
+IdEmpleado int,
+CONSTRAINT FK_IdCliente2 FOREIGN KEY (IdCliente) REFERENCES Cliente(IdCliente),
+CONSTRAINT FK_Refaccion  FOREIGN KEY (IdRefaccion) REFERENCES Refaccion(IdRefaccion),
+CONSTRAINT FK_IdServicio FOREIGN KEY (IdServicio) REFERENCES Servicios(IdServicio),
+CONSTRAINT FK_Empleado   FOREIGN KEY (IdEmpleado) REFERENCES Empleado(IdEmpleado)
+)
+
 
 --Factura
-insert into Factura(IdFactura,Fecha,Total,IdCliente,IdServicio,IdRefaccion) values(537,'2020-10-09',200.00,048,011,344);
-insert into Factura(IdFactura,Fecha,Total,IdCliente,IdServicio,IdRefaccion) values(538,'2020-10-09',350.00,048,012,411);
-insert into Factura(IdFactura,Fecha,Total,IdCliente,IdServicio,IdRefaccion) values(539,'2020-10-09',80.00,048,013,503);
+insert into Factura values(1,2020-10-04,14590,5,1,11,0323)
+insert into Factura values(2,2020-10-03,3500,4,2,12,0223)
+insert into Factura values(3,2020-09-27,20850,1,3,9,0123)
 
+select * from Cliente;
+select * from Automovil;
+select * from Departamentos;
+select * from Empleado;
+select * from Factura;
+select * from Refaccion;
+select * from Diagnostico;
+select * from Supervisor;
+select * from Servicios;
 
 
 
@@ -185,6 +251,17 @@ insert into Supervisor values (7358,'Miguel Angel', 'Paolino', '8503315463', 'Mp
 
 select * from Refaccion
 
+create table Refaccion (
+IdRefaccion int PRIMARY KEY NOT NULL,
+Nombre varchar (35),
+Costo float,
+FEntrada Datetime,
+FSalida Datetime,
+IdDepartamento int,
+CONSTRAINT FK_IdDepartamento_TBRefaccion FOREIGN KEY (IdDepartamento) REFERENCES Departamentos(IdDepartamento)
+)
+
+
 insert into Refaccion values (1,'Autolite Iridium Iridio Bujia',307, 2020-10-01,2020-10-04, 5)
 insert into Refaccion values (2,'Base Palanca De Cambios Ford F150',1190, '2020-10-01 07:00:25','2020-10-05 08:24:35' , 5)
 insert into Refaccion values (3,'Motor de Arranque Electrico',2943, '2020-10-01 07:00:25','2020-10-05 08:24:35' , 2)
@@ -195,5 +272,8 @@ insert into Refaccion values (7,'Amortiguadores Jetta Golf A2 A3' ,1824 , '2020-
 insert into Refaccion values (8,'Tensor Ajustable Muelle' ,1499 , '2020-10-04 07:00:25','2020-10-03 08:24:35' , 3)
 insert into Refaccion values (9,'Motor Dodge O Jeep V6' ,19850 , '2020-09-24 12:58:00','2020-09-28 16:24:00' , 4)
 insert into Refaccion values (10,'Motor Nissan Valvulas Pick Up' ,18650 , '2020-09-24 12:58:00','2020-09-28 16:24:00' , 4)
+insert into Refaccion values (11,'Suspension' ,9590, '2020-09-24 12:58:00','2020-09-28 16:24:00' , 3)
+insert into Refaccion values (12, 'focos',500,'2020-09-24 12:58:00','2020-10-03 16:24:00' , 2)
+
 
 /* Falta Factura */ 
