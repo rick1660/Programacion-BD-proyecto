d14c013ed1c0d533d2c0a9969b459ab5346dc892
Juan Negrete <john.carlos.116@gmail.com>
2020-12-10 16:39:43 -0800

    Agregue form Bitacora, Clases Conexion, Consulta 1 Consulta 2, triggers BD

---
+++ Version3_0/BDConcesionario.sql
@@ -0,0 +1,423 @@
+create database concesionario
+use concesionario
+
+
+--Sucursal principal
+CREATE TABLE Sucursal_principal
+(
+ IdSucursal INT PRIMARY KEY NOT NULL,
+ Nombre VARCHAR(45),
+ Direccion VARCHAR(45),
+ Telefono INT,
+ Correo VARCHAR(45),
+)
+
+--Empleados
+CREATE TABLE Empleados
+(
+  IdEmpleados INT PRIMARY KEY NOT NULL,
+  Nombre VARCHAR(45) ,
+  Apellidos VARCHAR(45) ,
+  FechaIngreso DATE ,
+  Telefono INT ,
+  Empleadoscol VARCHAR(45) ,
+  Sucursal INT NOT NULL,
+  CONSTRAINT FK_IdEmpleados_TBSucursalPrincipal FOREIGN KEY (IdEmpleados) REFERENCES Sucursal_principal (IdSucursal)
+)
+
+--Clientes
+CREATE TABLE Clientes 
+(
+  IdClientes INT PRIMARY KEY NOT NULL,
+  Nombre VARCHAR(45),
+  apellidos VARCHAR(45),
+  Telefono VARCHAR(45),
+  Correo VARCHAR(45),
+  Edad INT,
+  Direccion VARCHAR(45),
+  Sexo VARCHAR(45),
+  FechaNacimiento DATE,
+  RFC VARCHAR(45),
+  ReferenciaUno VARCHAR(45) NOT NULL,
+  ReferenciaDos VARCHAR(45) NOT NULL,
+  ReferenciaTres VARCHAR(45) NOT NULL,
+  NumRefUno INT NOT NULL,
+  NumRedDos INT NOT NULL,
+  NumRefTres INT NOT NULL,
+)
+
+--Automovil
+CREATE TABLE Automoviles 
+(
+  IdAutomovil INT PRIMARY KEY NOT NULL,
+  Marca VARCHAR(45) NOT NULL,
+  Modelo VARCHAR(45) NOT NULL,
+  Color VARCHAR(45) NOT NULL,
+  Año VARCHAR(45) NOT NULL,
+  Serie VARCHAR(25) NOT NULL,
+  FechaIngreso VARCHAR(45) NOT NULL,
+  PrecioCompra INT,
+  PrecioVenta INT,
+  PrecioTotal INT,
+  PrecioPromo INT,
+  Sucursal INT NOT NULL,
+  CONSTRAINT FK_SucursalAuto_TBSucursalPrincipal FOREIGN KEY (Sucursal) REFERENCES Sucursal_principal (IdSucursal)
+  )
+
+
+--Autopartes
+CREATE TABLE Autopartes 
+(
+  IdAutopartes INT PRIMARY KEY NOT NULL,
+  Nombre VARCHAR(45) NOT NULL,
+  Modelo VARCHAR(45) NOT NULL,
+  Cantidad INT NOT NULL,
+  Sucursal INT NOT NULL,
+  CONSTRAINT FK_SucursalPart_TBSucursalPrincipal FOREIGN KEY (Sucursal) REFERENCES Sucursal_principal (IdSucursal)
+  )
+
+--Sucursal secundaria
+CREATE TABLE Sucursal_Secundaria
+(
+  IdSucursal_Secundaria INT PRIMARY KEY NOT NULL,
+  Nombre VARCHAR(45) NOT NULL,
+  Direccion VARCHAR(45) NOT NULL,
+  Telefono INT NOT NULL,
+  Correo VARCHAR(45) NOT NULL,
+)
+
+--Traspasos
+CREATE TABLE TraspasoSaS
+(
+  IdTraspasoSaS INT PRIMARY KEY NOT NULL,
+  SucursalEntrega INT NOT NULL,
+  SucursalRecibe INT NOT NULL,
+  TipoTraspaso VARCHAR(45),
+  CONSTRAINT FK_SucursalRecibe_TBSucursal_principal  FOREIGN KEY (SucursalEntrega) REFERENCES Sucursal_principal (IdSucursal),
+  CONSTRAINT FK_SucursalEntrega_TBSucursal_Secunadaria  FOREIGN KEY (SucursalRecibe) REFERENCES Sucursal_Secundaria (IdSucursal_Secundaria)
+)
+
+--Compra venta
+CREATE TABLE Compraventa 
+(
+  IdCompraVenta INT PRIMARY KEY NOT NULL,
+  Cliente INT,
+  Fecha DATE,
+  TipoFinanciamiento VARCHAR(45),
+  Total Float,
+  CONSTRAINT FK_Cliente_TBDClientes   FOREIGN KEY (Cliente) REFERENCES Clientes (IdClientes)
+)
+
+--Detalle compra vente
+CREATE TABLE DetalleCompraVenta 
+(
+  IdDetalleCompraVenta INT PRIMARY KEY NOT NULL,
+  Automovil INT NOT NULL,
+  Cantidad INT NOT NULL,
+  precio VARCHAR(45) NOT NULL,
+  IdCompraventa INT NOT NULL,
+  Accesorios INT NOT NULL,
+  CONSTRAINT FK_ClienteDetalleCompraVenta_TBDClientes   FOREIGN KEY (Automovil) REFERENCES Automoviles (IdAutomovil),
+  CONSTRAINT FK_IdCompraventa_TBDCompraVenta   FOREIGN KEY (IdCompraventa) REFERENCES Compraventa (IdCompraVenta)
+  )
+
+CREATE TABLE DetalleTraspaso 
+(
+  idDetalle_Traspaso INT PRIMARY KEY NOT NULL,
+  IdTraspasoSAS INT NOT NULL,
+  Autoparte INT NOT NULL,
+  Cantidad INT NOT NULL,
+  Automovil INT NOT NULL,
+  CONSTRAINT FK_IdTraspasoSAS_TBDClientes   FOREIGN KEY (IdTraspasoSAS) REFERENCES TraspasoSaS (IdTraspasoSaS),
+  CONSTRAINT FK_Autoparte_TBDClientes   FOREIGN KEY (Autoparte) REFERENCES Autopartes (IdAutopartes),
+  CONSTRAINT FK_Automovil_TBDClientes   FOREIGN KEY (Automovil) REFERENCES Automoviles (IdAutomovil),
+)
+
+
+CREATE TABLE Promocion
+(
+   IdPromocion INT PRIMARY KEY NOT NULL,
+   Cantidad_Autos INT,
+   Descuento INT,
+   FechaVigencia DATE
+)
+
+
+CREATE TABLE Rol
+(
+  IdRol INT PRIMARY KEY NOT NULL,
+  Tipo VARCHAR(10),
+  Contraseña VARCHAR(10),
+)
+
+
+CREATE TABLE Accesorios
+(
+ IdAccesorios INT PRIMARY KEY NOT NULL,
+ Nombre VARCHAR(20),
+ Descripcion VARCHAR(45),
+ IdAutomovil INT NOT NULL,
+)
+
+--TRIGGERS
+/*Selección de un automóvil y que permita llevar un proceso de aplicación de accesorios 
+al seleccionar un vehículo con características básicas*/
+create trigger SeleccionVehiculo
+
+
+/*Cancelación de una compra que se encontraba en un proceso avanzado de adquisición.*/
+create trigger cancelar
+
+
+/*Modificaciónde las características particulares de un vehículo cuando se encuentra 
+en un proceso de compra-venta y este realiza el cambio de todas las características...*/
+create trigger modificar
+
+
+/*Realizar una auditoría de los vendedores de autos, el cual contenga, vendedor, automóvil, 
+si  este  tuvo  equipamiento especial o  no,  si  se  le  aplica  alguna  promoción,  si  fue
+solo 1 auto o flotilla (aplica a partir de 2 autos en adelante), monto final de la venta, 
+comisión por la apertura del crédito y hora/fecha de la transacción.*/
+create trigger auditoria
+
+
+/*Proceso que permita registra un nuevo modelo de automóvil que nunca haya estado en el 
+inventario.*/
+create trigger registrar
+
+
+--TABLAS PARA TRIGGER
+create table BitacoraComprasVentas (
+ IdCompraVenta INT,
+ Cliente INT,
+ TipoFinanciamiento VARCHAR(45),
+ Total Float,
+  Fecha DATETime,
+  Accion varchar(20)
+)
+
+create table BitacoraTraspasos (
+  IdTraspasoSaS INT,
+  Rol varchar(15),
+  SucursalEntrega INT NOT NULL,
+  SucursalRecibe INT NOT NULL,
+  TipoTraspaso VARCHAR(45),
+  Fecha DateTime,
+  Accion varchar(20)
+)
+
+create table BitacoraClientes (
+Rol varchar(15),
+Fecha DateTime,
+Accion varchar(20)
+)
+
+create table BitacoraEmpleados (
+Rol varchar(15),
+Fecha DateTime,
+Accion varchar(20)
+)
+
+create table BitacoraAutos (
+Rol varchar(15),
+Fecha DateTime,
+Accion varchar(20)
+)
+
+create table BitacoraAccesorios (
+Rol varchar(15),
+Fecha DateTime,
+Accion varchar(20)
+)
+
+
+------------
+create trigger tr_BitacoraClientesAlta
+on Clientes for insert
+as
+declare @Fecha DateTime
+
+set @Fecha = (select GETDATE())
+insert into BitacoraClientes ( Rol, Fecha, Accion)
+values ('Administrador',@Fecha, 'Alta de Cliente Nuevo' )
+go
+
+
+
+create trigger tr_BitacoraClientesBaja
+on Clientes for delete
+as
+declare @Fecha DateTime
+
+set @Fecha = (select GETDATE())
+insert into BitacoraClientes ( Rol, Fecha, Accion)
+values ('Administrador',@Fecha, 'Eliminacion de Cliente' )
+go
+
+
+create trigger tr_BitacoraClientesActualizacion
+on Clientes for update
+as
+declare @Fecha DateTime
+
+set @Fecha = (select GETDATE())
+insert into BitacoraClientes ( Rol, Fecha, Accion)
+values ('Administrador',@Fecha, 'Actualizacion de Datos de Cliente' )
+go
+
+
+---------------
+create trigger tr_BitacoraEmpleadosAlta
+on Empleados for insert
+as
+declare @Fecha DateTime
+
+set @Fecha = (select GETDATE())
+insert into BitacoraEmpleados ( Rol, Fecha, Accion)
+values ('Administrador',@Fecha, 'Alta de Empleado Nuevo' )
+go
+
+
+create trigger tr_BitacoraEmpleadosBaja
+on Empleados for delete
+as
+declare @Fecha DateTime
+
+set @Fecha = (select GETDATE())
+insert into BitacoraEmpleados ( Rol, Fecha, Accion)
+values ('Administrador',@Fecha, 'Eliminacion de Empleado' )
+go
+
+
+create trigger tr_BitacoraEmpleadosActualizacion
+on Empleados for update
+as
+declare @Fecha DateTime
+
+set @Fecha = (select GETDATE())
+insert into BitacoraEmpleados( Rol, Fecha, Accion)
+values ('Administrador',@Fecha, 'Actualizacion de Datos de Empleado' )
+go
+
+--------------------
+
+create trigger tr_BitacoraAutosAlta
+on Automoviles for insert
+as
+declare @Fecha DateTime
+
+set @Fecha = (select GETDATE())
+insert into BitacoraAutos( Rol, Fecha, Accion)
+values ('Administrador',@Fecha, 'Alta de Automovil' )
+go
+
+
+create trigger tr_BitacoraAutosBaja
+on Automoviles for delete
+as
+declare @Fecha DateTime
+
+set @Fecha = (select GETDATE())
+insert into BitacoraAutos ( Rol, Fecha, Accion)
+values ('Administrador',@Fecha, 'Eliminacion de Automovil' )
+go
+
+
+create trigger tr_BitacoraAutosActualizacion
+on Automoviles for update
+as
+declare @Fecha DateTime
+
+set @Fecha = (select GETDATE())
+insert into BitacoraAutos( Rol, Fecha, Accion)
+values ('Administrador',@Fecha, 'Actualizacion de Datos de Automovil' )
+go
+
+------------------------------
+
+create trigger tr_BitacoraAccesoriosAlta
+on Accesorios for insert
+as
+declare @Fecha DateTime
+
+set @Fecha = (select GETDATE())
+insert into BitacoraAccesorios( Rol, Fecha, Accion)
+values ('Administrador',@Fecha, 'Alta de Accesorio' )
+go
+
+
+create trigger tr_BitacoraAccesoriosBaja
+on Accesorios for delete
+as
+declare @Fecha DateTime
+
+set @Fecha = (select GETDATE())
+insert into BitacoraAccesorios ( Rol, Fecha, Accion)
+values ('Administrador',@Fecha, 'Eliminacion de Automovil' )
+go
+
+
+create trigger tr_BitacoraAccesoriosActualizacion
+on Accesorios for update
+as
+declare @Fecha DateTime
+
+set @Fecha = (select GETDATE())
+insert into BitacoraAccesorios( Rol, Fecha, Accion)
+values ('Administrador',@Fecha, 'Actualizacion de Datos de Automovil' )
+go
+
+
+-----------------------
+create trigger tr_BitacoraTraspasos
+on TraspasoSaS for insert
+as
+declare @IdTraspasoSaS int
+declare @Rol varchar(15)
+declare @SucursalEntrega INT 
+declare @SucursalRecibe INT 
+declare @TipoTraspaso VARCHAR(45)
+declare @Fecha DateTime
+declare @Accion varchar(20)
+
+
+set @IdTraspasoSaS = (select IdTraspasoSaS from Inserted)
+set @SucursalEntrega = (select SucursalEntrega from Inserted)
+set @SucursalRecibe = (select SucursalRecibe from Inserted)
+set @TipoTraspaso = (select TipoTraspaso from Inserted)
+set @Fecha = (select GETDATE())
+
+insert into BitacoraTraspasos( IdTraspasoSaS, Rol, SucursalEntrega, SucursalRecibe, TipoTraspaso, Fecha, Accion)
+values (@IdTraspasoSaS,'Administrador', @SucursalEntrega, @SucursalRecibe, @TipoTraspaso, @Fecha, 'Alta de Traspaso Nuevo' )
+go
+
+
+
+create trigger tr_BitacoraCompraVenta
+on CompraVenta for insert
+as
+declare @IdCompraVenta int
+declare @Cliente int
+declare @TipoFinanciamiento varchar(45)
+declare @Total float
+declare @Fecha datetime
+declare @Accion varchar(20)
+
+set @Fecha = (select GETDATE())
+insert into BitacoraComprasVentas( IdCompraVenta, Cliente, TipoFinanciamiento, Total, Fecha, Accion)
+values (@IdCompraVenta,@Cliente, @TipoFinanciamiento, @Total , @Fecha, 'Registro de Compra Venta' )
+go
+
+------- PROCEDIMIENTOS ALMACENADOS
+
+-- BITACORA
+create proc MostrarBitacoraCompraVenta
+as
+select *from BitacoraComprasVentas
+go
+
+create proc MostrarBitacoraTraspasos
+as
+select *from BitacoraTraspasos
+go
+
--- VisualConcesionaria/Concesionaria/Concesionaria/Form1.Designer.cs
+++ VisualConcesionaria/Concesionaria/Concesionaria/Form1.Designer.cs
@@ -29,23 +29,23 @@
         private void InitializeComponent()
         {
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
-            this.button5 = new System.Windows.Forms.Button();
-            this.button6 = new System.Windows.Forms.Button();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
             this.groupBox3 = new System.Windows.Forms.GroupBox();
-            this.button7 = new System.Windows.Forms.Button();
-            this.button8 = new System.Windows.Forms.Button();
-            this.button9 = new System.Windows.Forms.Button();
-            this.button10 = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
             this.groupBox3.SuspendLayout();
@@ -69,8 +69,109 @@
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
             // 
+            // button10
+            // 
+            this.button10.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button10.Location = new System.Drawing.Point(26, 516);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(227, 49);
+            this.button10.TabIndex = 10;
+            this.button10.Text = "&Reportes";
+            this.button10.UseVisualStyleBackColor = true;
+            // 
+            // button9
+            // 
+            this.button9.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button9.Location = new System.Drawing.Point(26, 406);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(227, 49);
+            this.button9.TabIndex = 8;
+            this.button9.Text = "&Traspasos";
+            this.button9.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            this.button8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button8.Location = new System.Drawing.Point(26, 351);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(227, 49);
+            this.button8.TabIndex = 9;
+            this.button8.Text = "&Accesorios";
+            this.button8.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button7.Location = new System.Drawing.Point(26, 241);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(227, 49);
+            this.button7.TabIndex = 8;
+            this.button7.Text = "&Compra";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button6.Location = new System.Drawing.Point(26, 461);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(227, 49);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "&Bitacora";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.Location = new System.Drawing.Point(26, 296);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(227, 49);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "&Venta";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(26, 186);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(227, 49);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "&Administrador";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(26, 131);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(227, 49);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "&Empleados";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(26, 76);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(227, 49);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "&Clientes";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(26, 21);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(227, 49);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "&Autos";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // label1
             // 
+            this.label1.BackColor = System.Drawing.SystemColors.AppWorkspace;
             this.label1.Dock = System.Windows.Forms.DockStyle.Top;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.Location = new System.Drawing.Point(0, 0);
@@ -116,66 +217,6 @@
             this.label5.TabIndex = 5;
             this.label5.Text = "Sucursal:";
             // 
-            // button1
-            // 
-            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(26, 21);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(227, 49);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "&Autos";
-            this.button1.UseVisualStyleBackColor = true;
-            // 
-            // button2
-            // 
-            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(26, 76);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(227, 49);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "&Clientes";
-            this.button2.UseVisualStyleBackColor = true;
-            // 
-            // button3
-            // 
-            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button3.Location = new System.Drawing.Point(26, 131);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(227, 49);
-            this.button3.TabIndex = 2;
-            this.button3.Text = "&Empleados";
-            this.button3.UseVisualStyleBackColor = true;
-            // 
-            // button4
-            // 
-            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button4.Location = new System.Drawing.Point(26, 186);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(227, 49);
-            this.button4.TabIndex = 3;
-            this.button4.Text = "&Administrador";
-            this.button4.UseVisualStyleBackColor = true;
-            // 
-            // button5
-            // 
-            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button5.Location = new System.Drawing.Point(26, 296);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(227, 49);
-            this.button5.TabIndex = 4;
-            this.button5.Text = "&Venta";
-            this.button5.UseVisualStyleBackColor = true;
-            // 
-            // button6
-            // 
-            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button6.Location = new System.Drawing.Point(26, 461);
-            this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(227, 49);
-            this.button6.TabIndex = 5;
-            this.button6.Text = "&Bitacora";
-            this.button6.UseVisualStyleBackColor = true;
-            // 
             // groupBox2
             // 
             this.groupBox2.Controls.Add(this.label3);
@@ -195,46 +236,6 @@
             this.groupBox3.Size = new System.Drawing.Size(220, 74);
             this.groupBox3.TabIndex = 7;
             this.groupBox3.TabStop = false;
-            // 
-            // button7
-            // 
-            this.button7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button7.Location = new System.Drawing.Point(26, 241);
-            this.button7.Name = "button7";
-            this.button7.Size = new System.Drawing.Size(227, 49);
-            this.button7.TabIndex = 8;
-            this.button7.Text = "&Compra";
-            this.button7.UseVisualStyleBackColor = true;
-            // 
-            // button8
-            // 
-            this.button8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button8.Location = new System.Drawing.Point(26, 351);
-            this.button8.Name = "button8";
-            this.button8.Size = new System.Drawing.Size(227, 49);
-            this.button8.TabIndex = 9;
-            this.button8.Text = "&Accesorios";
-            this.button8.UseVisualStyleBackColor = true;
-            // 
-            // button9
-            // 
-            this.button9.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button9.Location = new System.Drawing.Point(26, 406);
-            this.button9.Name = "button9";
-            this.button9.Size = new System.Drawing.Size(227, 49);
-            this.button9.TabIndex = 8;
-            this.button9.Text = "&Traspasos";
-            this.button9.UseVisualStyleBackColor = true;
-            // 
-            // button10
-            // 
-            this.button10.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button10.Location = new System.Drawing.Point(26, 516);
-            this.button10.Name = "button10";
-            this.button10.Size = new System.Drawing.Size(227, 49);
-            this.button10.TabIndex = 10;
-            this.button10.Text = "&Reportes";
-            this.button10.UseVisualStyleBackColor = true;
             // 
             // frmMenuPrincipal
             // 
